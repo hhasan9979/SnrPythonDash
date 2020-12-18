1f62f74767460ee246cbe58c0e65812587f34c59
hhasan9979 <hhasan9979@gmail.com>
2020-12-18 12:29:58 -0500

    make it better

--- 2019-2020CovidPlayoffs.py
+++ 2019-2020CovidPlayoffs.py
@@ -2,9 +2,52 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly
 import plotly.express as px
-
+import plotly.io as pio
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+from IPython.display import display, Markdown
+from math import pi
+import seaborn as sns
+
+########
+dff = pd.read_csv("NBAPlayerWinProbability1.csv")
+#dff = dff[dff['state_name']=='Maharashtra']
+dff = dff.groupby(['Team', 'Player'],as_index=False)[['WPA','SH','TO','FT']].sum()
+print (dff[:5])
+
+
+barchart = px.bar(
+    data_frame=dff,
+    x="Team",
+    y="WPA",
+    color="Player",               # differentiate color of marks
+    opacity=0.9,                  # set opacity of markers (from 0 to 1)
+    orientation="v",              # 'v','h': orientation of the marks
+    barmode='relative',           # in 'overlay' mode, bars are top of one another.
+                                  # in 'group' mode, bars are placed beside each other.
+                                  # in 'relative' mode, bars are stacked above (+) or below (-) zero.
+    #----------------------------------------------------------------------------------------------
+
+
+    labels={"WPA":"Win Probability Added",
+    "Player":"Player"},           # map the labels of the figure
+    title='Win Probability Added for Each Team', # figure title
+    width=1400,                   # figure width in pixels
+    height=720,                   # figure height in pixels
+    template='presentation',            # 'ggplot2', 'seaborn', 'simple_white', 'plotly',
+                                  # 'plotly_white', 'plotly_dark', 'presentation',
+
+
+#pio.show(barchart)
+)
+########
 
 df = pd.read_csv('https://raw.githubusercontent.com/hhasan9979/hhasan9979.github.io/main/PlayoffStatsPre%26PostBubble.csv')
 goats = ['Michael Jordan','Lebron James','Magic Johnson','Larry Bird','Tim Duncan','Shaquille']
@@ -28,7 +71,22 @@ app.layout = html.Div([
             marks={str(Year): str(Year) for Year in df['Num'].unique()},
             step=None
         )
-    ])
+    ]),
+    html.Div([
+            dcc.Dropdown(id='linedropdown',
+                options=[
+                         {'label': 'Deaths', 'value': 'deaths'},
+                         {'label': 'Cases', 'value': 'cases'}
+                ],
+                value='deaths',
+                multi=False,
+                clearable=False
+            ),
+        ],className='six columns'),
+
+    html.Div([
+            dcc.Graph(figure=barchart)
+        ])
 ])
 
 @app.callback(
